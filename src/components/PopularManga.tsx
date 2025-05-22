24947052ce0f9924335cf295dc8ba3d776cc7a18
emanh <cnttblog@gmail.com>
2025-05-22 17:18:23 +0700

    feat: wrap MangaCard in Link for PopularManga component

--- src/components/PopularManga.tsx
+++ src/components/PopularManga.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getTopManga } from "../api/jikan";
 import type { Manga } from "../types/manga";
 import MangaCard from "./MangaCard";
+import { Link } from "react-router-dom";
 
 const PopularManga: React.FC = () => {
   const [popular, setPopular] = useState<Manga[]>([]);
@@ -28,7 +29,9 @@ const PopularManga: React.FC = () => {
       <h2 className="text-2xl font-semibold mb-4">Popular Manga</h2>
       <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
         {popular.map((manga) => (
-          <MangaCard key={manga.mal_id} manga={manga} />
+            <Link to={`/manga/${manga.mal_id}`} key={manga.mal_id}>
+              <MangaCard manga={manga} />
+            </Link>
         ))}
       </div>
     </section>
