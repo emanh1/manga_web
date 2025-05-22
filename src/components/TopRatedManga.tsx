87051d3029a2b2325b41e61df556f2ce73138019
emanh <cnttblog@gmail.com>
2025-05-22 19:27:18 +0700

    fix: increase getTopManga to get 20

--- src/components/TopRatedManga.tsx
+++ src/components/TopRatedManga.tsx
@@ -12,7 +12,7 @@ const TopRatedManga: React.FC = () => {
     async function fetchTopRated() {
       setLoading(true);
       try {
-        const data = await getTopManga(8);
+        const data = await getTopManga(20);
         setTopRated(data);
       } catch {
         setTopRated([]);
@@ -27,12 +27,18 @@ const TopRatedManga: React.FC = () => {
   return (
     <section className="mt-6">
       <h2 className="text-2xl font-semibold mb-4">Top Rated Manga</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
-        {topRated.map((manga) => (
-            <Link to={`/manga/${manga.mal_id}`} key={manga.mal_id}>
-              <MangaCard manga={manga} />
-            </Link>
-        ))}
+      <div className="relative">
+        <div className="overflow-x-auto pb-4">
+          <div className="flex gap-4 w-max">
+            {topRated.map((manga) => (
+              <div className="w-[250px] flex-shrink-0" key={manga.mal_id}>
+                <Link to={`/manga/${manga.mal_id}`}>
+                  <MangaCard manga={manga} />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
