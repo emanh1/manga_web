a97fe1ecf5f3ec857262e94d4aebf66b27152352
emanh <cnttblog@gmail.com>
2025-05-23 22:18:17 +0700

    refactor: move app.tsx to setup

--- src/setup/App.tsx
+++ src/setup/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import MangaDetails from "./components/MangaDetails";
+import Layout from "../components/Layout";
+import Home from "../pages/Home";
+import MangaDetails from "../components/MangaDetails";
 
 function App() {
   return (
