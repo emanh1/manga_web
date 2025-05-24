5616dfdb46dbae48b2bba594d1006ea1a7e008d8
emanh <cnttblog@gmail.com>
2025-05-24 21:25:59 +0700

    feat: add backend axios instance

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+//TODO use dotenv
+const API_BASE_URL = 'http://localhost:3000/api';
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) { //expired, invalid
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
