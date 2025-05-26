1a2a9ce506f2e99ea7b3c6b8541cc276515d35d2
emanh <cnttblog@gmail.com>
2025-05-26 21:23:27 +0700

    fix: use readstream for files instead of File[]

--- backend/utils/ipfsClient.ts
+++ backend/utils/ipfsClient.ts
@@ -1,10 +1,12 @@
 import { create } from 'kubo-rpc-client';
+import fs from 'fs';
 
 const client = create({url: 'http://localhost:5001/api/v0'}); //TODO: create from a database of known nodes
 
-export const uploadFiles = async (files: File[]): Promise<string[]> => {
+export const uploadFilesToIPFS = async (files): Promise<string[]> => {
   const results: string[] = [];
   for (const file of files) {
+    const stream = fs.createReadStream(file.path);
     const { cid } = await client.add(file);
     results.push(cid.toString());
   }
