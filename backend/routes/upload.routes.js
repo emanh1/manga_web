53d58e3c22484669bb3453d9b2ef7fc81cc45edd
emanh <cnttblog@gmail.com>
2025-06-15 20:56:32 +0700

    extract backend to separate repository

--- backend/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { uploadTitleChapter, upload } from '../controllers/upload.controller.js';
-import { verifyToken } from '../middlewares/auth.js';
-import { uploadValidation } from '../middlewares/validation.js';
-const router = express.Router();
-
-router.post('/', verifyToken, upload, uploadValidation, uploadTitleChapter);
-
-export default router;
